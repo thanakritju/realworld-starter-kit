89c145dc416feeb181418ffab171e0e5ec2f4187
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-10-15 22:12:33 +0700

    Add home

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
       <Footer />
     </div>
   );
