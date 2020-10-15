42a150ff5097ca27184983f5fc7655f27fab4371
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-10-15 22:31:09 +0700

    More refactor

---
+++ react-redux/src/components/Home/PopularTags.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const PopularTags = () => {
+  return (
+    <div class="col-md-3">
+      <div class="sidebar">
+        <p>Popular Tags</p>
+        <div class="tag-list">
+          <a href="" class="tag-pill tag-default">programming</a>
+          <a href="" class="tag-pill tag-default">javascript</a>
+          <a href="" class="tag-pill tag-default">emberjs</a>
+          <a href="" class="tag-pill tag-default">angularjs</a>
+          <a href="" class="tag-pill tag-default">react</a>
+          <a href="" class="tag-pill tag-default">mean</a>
+          <a href="" class="tag-pill tag-default">node</a>
+          <a href="" class="tag-pill tag-default">rails</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PopularTags;
