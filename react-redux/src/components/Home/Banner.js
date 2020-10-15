9721154aee956c011f6ffd6cdb01b36e6d5365cb
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-10-15 22:25:48 +0700

    Extract Home

---
+++ react-redux/src/components/Home/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- react-redux/src/components/Home/index.js
+++ react-redux/src/components/Home/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
   return (
     <div class="home-page">
-      <div class="banner">
-        <div class="container">
-          <h1 class="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
-        </div>
-      </div>
+      <Banner />
       <div class="container page">
         <div class="row">
           <div class="col-md-9">
