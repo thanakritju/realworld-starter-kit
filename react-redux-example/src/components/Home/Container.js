aadf2576ffcc1067b7538ec3a9fcbb8abfe17ea4
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-10-17 13:32:57 +0700

    Refactor ArticleList

--- react-redux-example/src/components/Home/Container.js
+++ react-redux-example/src/components/Home/Container.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import PopularTags from './PopularTags';
-import Article from './Article';
-import FeedToggle from './FeedToggle';
+import ArticleList from './ArticleList';
 
 const Container = () => {
   return (
 	<div class="container page">
 		<div class="row">
-			<div class="col-md-9">
-				<FeedToggle />
-				<Article 
-					info={{ image: "http://i.imgur.com/Qr71crq.jpg", author: "Eric Simons", date: "January 20th"}}
-					heart={29}
-					preview={{ header: "How to build webapps that scale", description: "This is the description for the post."}}
-					/>
-				<Article 
-					info={{ image: "http://i.imgur.com/N4VcUeJ.jpg", author: "Albert Pai", date: "January 20th"}}
-					heart={32}
-					preview={{ header: "The song you won't ever stop singing. No matter how hard you try.", description: "This is the description for the post."}}
-					/>
-			</div>
+			<ArticleList />
 			<PopularTags />
 		</div>
 	</div>
