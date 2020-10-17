12a707fabf7c07a32f02e0d1867d36eae815897f
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-10-17 13:45:31 +0700

    refactor app

--- react-redux-example/src/components/App.js
+++ react-redux-example/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Provider } from "react-redux";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home/index';
-import { store } from './store';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home/index';
+import { store } from '../store';
 
 const App = () => {
   return (
