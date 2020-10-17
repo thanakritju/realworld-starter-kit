7a0bb41fd8380e9912c705ba8e85e0a94b15f1b1
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-10-17 13:14:10 +0700

    init redux

--- react-redux-example/src/App.js
+++ react-redux-example/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Provider } from "react-redux";
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home/index';
+import { store } from './store';
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <Header />
       <Home />
       <Footer />
-    </div>
+    </Provider>
   );
 }
 
---
+++ react-redux-example/src/reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import articleList from './reducers/articleList'
+
+export default combineReducers({
+  articleList
+});
